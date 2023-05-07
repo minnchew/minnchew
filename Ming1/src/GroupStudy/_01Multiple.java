dbb85b103d26a13ec2d289a3aa341d04e4acd053
minnchew <minnchew@gmail.com>
2023-05-07 16:44:10 +0900

    0503Project

---
+++ Ming1/src/GroupStudy/_01Multiple.java
@@ -0,0 +1,15 @@
+//Java학습 구구단 프로그램 개발 2023.05.03(수)
+package GroupStudy;
+
+public class _01Multiple {
+    public static void main(String [] args) {
+        for (int i = 2; i < 10; i++) {
+            for (int j = 1; j < 10; j++) {
+                System.out.println(i + "x" + j + "=" + (i * j));
+            }
+            System.out.println();
+        }
+
+    }}
+
+
